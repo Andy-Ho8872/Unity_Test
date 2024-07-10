cb45f4ca8d06bf9e7145b516f6295b44353f4482
AndyHo <h880702520@gmail.com>
2024-07-10 22:45:47 +0800

    add  singleton  method into project

---
+++ Assets/Universal/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Create a Singleton that can be globally accessible and doesn't require a reference in the project
+    public static GameManager Instance; 
+    // Other references
+    public AudioManager audioManager;
+    private void Awake() {
+        if (Instance != null && Instance != this) Destroy(Instance);
+        else Instance = this;
+    }
+}
