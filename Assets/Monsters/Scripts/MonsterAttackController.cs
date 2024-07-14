4118531326ed4d5ddd8bf86171790014c99475c1
AndyHo <h880702520@gmail.com>
2024-07-14 21:56:52 +0800

    refactor functions about bullets

--- Assets/Monsters/Scripts/MonsterAttackController.cs
+++ Assets/Monsters/Scripts/MonsterAttackController.cs
@@ -4,40 +4,24 @@ using UnityEngine;
 
 public class MonsterAttackController : MonoBehaviour
 {
-    public GameObject MonsterPrefab;
-    public Vector3 MonsterPosition;
-    public SpriteRenderer MonsterSprite;
+    public GameObject Player;
+    public Monster MonsterPrefab;
     public GameObject MonsterAttackPrefab;
-    public bool isShootingRight;
-    public float shootRange = 0.5f;
-    public float offsetX = 1f;
+    public Vector3 MonsterPosition;
+
     public float offsetY = 1.25f;
     public float attackCoolDown = 0.75f;
     private void Start()
     {
-        MonsterPrefab = GameObject.Find("Goblin"); // Monster game object
-        MonsterSprite = MonsterPrefab.GetComponent<SpriteRenderer>();
-        setShootingStatus();
+        Player = GameObject.Find("Player");
     }
     void Update()
     {
         setMonsterPosition();
-        shoot();
-    }
-    public void setShootingStatus()
-    {
-        // If monster is facing left
-        if (MonsterSprite.flipX) isShootingRight = false;
-        else isShootingRight = true;
-    }
-    public void shoot()
-    {
-        if (isShootingRight) MonsterAttackPrefab.transform.position += new Vector3(shootRange * Time.deltaTime * 40, 0, 0);
-        else MonsterAttackPrefab.transform.position -= new Vector3(shootRange * Time.deltaTime * 40, 0, 0);
     }
     public void setMonsterPosition()
     {
-        MonsterPosition = MonsterPrefab.transform.position;
+        if (MonsterPrefab) MonsterPosition = MonsterPrefab.transform.position;
     }
     // Create bullets
     public void createBullet()
@@ -46,13 +30,17 @@ public class MonsterAttackController : MonoBehaviour
         Vector3 newPosition = new Vector3(MonsterPosition.x, MonsterPosition.y - offsetY, MonsterPosition.z);
         if (attackCoolDown <= 0)
         {
-            GameObject bullet = Instantiate(MonsterAttackPrefab, newPosition, Quaternion.identity);
+            // Create bullets
+            GameObject bulletPrefab = Instantiate(MonsterAttackPrefab, newPosition, Quaternion.identity);
+            // Sets the owner of the current bullet
+            Bullet bulletScript = bulletPrefab.GetComponent<Bullet>();
+            if (bulletScript != null) bulletScript.setOwner(MonsterPrefab);
             // Add sound effect
             GameManager.Instance.audioManager.playAudioClip(3, "monster_attack", false);
             // Reset timer
             attackCoolDown = 0.75f;
             // Delete bullet
-            Destroy(bullet, 2f);
+            Destroy(bulletPrefab, 2f);
         }
     }
 }
